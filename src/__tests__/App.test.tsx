b1a485090ff5c19798dbb7157f62e007f972439c
Julian Pagtama <jpagtama@gmail.com>
2023-06-28 12:28:46 -0700

    add vitest config and test file

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import App from '../App';
+
+describe('App', () => {
+    it('checks if text exists', () => {
+        render(<App />);
+        const text = screen.getByText(/Hello World/i);
+        expect(text).toBeDefined();
+    })
+})
