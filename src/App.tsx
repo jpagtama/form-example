8800173b421082a8150e10352908501c99cd1db1
Julian Pagtama <jpagtama@gmail.com>
2023-07-06 11:48:06 -0700

    Add header, button, link to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,44 @@
-import './App.css';
+import { useState } from 'react';
 import Form from './components/Form';
+import './App.css';
 
 export default function App() {
+  const [mode, setMode] = useState('dark');
 
-  const styles = {
+  const modeHandler = () => {
+    switch (mode) {
+      case 'dark':
+        setMode('light');
+        break;
+      case 'light':
+        setMode('dark');
+        break;
+      default: setMode('dark');
+    }
+  }
+
+  const modeButton = {
+    "minWidth": "5rem",
+    "padding": "0.5rem 1rem",
+    "border": "none",
+    "borderRadius": "5px",
+    "backgroundColor": "var(--primary-color)",
+    "color": "var(--text-color)",
     "alignSelf": "center",
-    "paddingBottom": "2rem",
-    "fontSize": "clamp(3rem, 1rem + 10vw, 5rem)"
+    "cursor": "pointer"
+  }
+
+  const linkStyles = {
+    "alignSelf": "center"
   }
 
   return (
-    <div className={`app lightMode`}>
-      <h1 style={styles} >Form Example</h1>
+    <div className={`app ${mode}Mode`}>
+      <header >
+        <h1 >Form Example</h1>
+      </header>
+      <button style={modeButton} onClick={modeHandler} >{`${mode === 'dark' ? 'light' : 'dark'} mode`}</button>
+      <a style={linkStyles} href="">this is a link</a>
       <Form />
     </div>
   )
